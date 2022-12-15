a63d10e9d3d05895e7b12b5ff8061a876e6c57d0
jmclella <jmclella@fandm.edu>
2022-12-15 16:38:49 -0500

    Added Javascript functionality to add books
    Implemented Book Object that allows form entries
    to be converted into objects and appended into
    the DOM via javascript functionality

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* MODAL FUNCTIONALITY */
 
 // Get Modal
 var modal = document.getElementById("modal");
@@ -10,8 +11,84 @@ btn.onclick = function() {
     modal.style.display = "block";
 }
 
+// Exit modal when clicked outside of form
 window.onclick = function(event) {
     if(event.target == modal) {
         modal.style.display = "none";
     }
 }
+
+
+
+/* LIBRARY FUNCTIONALITY */
+
+// Declare form and submit variables
+form = document.getElementById('form');
+submitButton = document.getElementById('submit');
+bookGrid = document.getElementById('bookGrid');
+
+// Declare library list of books
+let myLibrary = []
+
+// Create function when submit button is clicked to add Book
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    createBook()
+});
+
+// Create book object and add to library
+function createBook() {
+    title = document.getElementById('title').value;
+    author = document.getElementById('author').value;
+    pages = document.getElementById('pages').value;
+    isRead = document.getElementById('isRead').checked;
+
+    tempBook = new Book(title, author, pages, isRead);
+    addBookToLibrary(tempBook);
+}
+
+// Create Book Object
+function Book (title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+}
+
+// Push book into library and update DOM
+function addBookToLibrary (book) {
+    myLibrary.push(book);
+    updateLibrary();
+}
+
+// Create DOM Elements and loop through books to add to library
+function updateLibrary() {
+    for (book of myLibrary) {
+        let bookCard = document.createElement('div');
+        bookCard.classList.add('book-card');
+        let titleElem = document.createElement('p');
+        let authorElem = document.createElement('p');
+        let pagesElem = document.createElement('p');
+        let isReadElem = document.createElement('p');
+
+        titleElem.textContent = book.title;
+        authorElem.textContent = book.author;
+        pagesElem.textContent = book.pages;
+        isReadElem.textContent = book.isRead;
+
+        bookCard.appendChild(titleElem);
+        bookCard.appendChild(authorElem);
+        bookCard.appendChild(pagesElem);
+        bookCard.appendChild(isReadElem);
+        bookGrid.appendChild(bookCard);
+    }
+}
+
+
+
+
+
+
+
+
+
